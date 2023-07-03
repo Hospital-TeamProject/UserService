f51d708938e3c6c1d635aa1e703fe52f65b5714f
MatejaMusa <matejamusa6@gmail.com>
2023-07-03 17:30:07 +0200

    add hospital repository

---
+++ src/main/java/hospital/userservice/repository/HospitalRepository.java
@@ -0,0 +1,9 @@
+package hospital.userservice.repository;
+
+import hospital.userservice.model.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+}
