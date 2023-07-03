dcdff364d5b4ae6c51bf633e367b63de66164f07
MatejaMusa <matejamusa6@gmail.com>
2023-07-03 16:53:27 +0200

    change user repository path

--- src/main/java/hospital/userservice/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package hospital.userservice.model.repository;
-
-import hospital.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
