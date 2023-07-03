a11d654850116d8ea14458ce2d6ade6dc16a1585
MatejaMusa <matejamusa6@gmail.com>
2023-07-03 17:40:29 +0200

    add department model and change ddl-auto to update

---
+++ src/main/java/hospital/userservice/model/Department.java
@@ -0,0 +1,23 @@
+package hospital.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private UUID pbo;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "hospital_id", referencedColumnName = "id", nullable = false)
+    private Hospital hospital;
+}
