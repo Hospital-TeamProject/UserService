6d08a87e1f06c0fd14e52b1198878cbe95964064
MatejaMusa <77113780+MatejaMusa@users.noreply.github.com>
2023-07-04 12:52:47 +0200

    Model (#2)
    Add Hospital and Department model and repository

---
+++ src/main/java/hospital/userservice/model/Hospital.java
@@ -0,0 +1,26 @@
+package hospital.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private UUID pbb;
+    @Column(nullable = false)
+    private String fullName;
+    @Column(nullable = false)
+    private String shortName;
+    @Column(nullable = false)
+    private String place;
+    @Column(nullable = false)
+    private String address;
+}
